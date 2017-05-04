cf41631bb473c59522f1cfbf4b02a36a4ff873a9
qq986945193 <986945193@qq.com>
2017-05-04 10:50:52 +0800

    commit dandroidutils

--- dandroidlibrary/src/main/java/com/david/dandroidlibrary/utils/constants/Constant.java
+++ dandroidlibrary/src/main/java/com/david/dandroidlibrary/utils/constants/Constant.java
@@ -1,9 +1,14 @@
 package com.david.dandroidlibrary.utils.constants;
+/*
+ * @Author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ * @OsChina空间: https://my.oschina.net/mcxiaobing
+ */
 
 /**
- * Created by david zheng on 2017/4/22.
- * <p>
- * http://www.anenda.com
+ * Created by david zheng
  */
 
 public class Constant {
